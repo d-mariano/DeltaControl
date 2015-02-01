cbf01a81ea38536b9323553d2a73c539d8c32f86
Dave Mariano <dave.mariano1991@gmail.com>
2015-02-01 17:39:29 -0500

    Major updates in pyDeltaServer.py, asynchronous server implemented

--- pyDeltaServer/pyDeltaServer.py
+++ pyDeltaServer/pyDeltaServer.py
@@ -12,11 +12,11 @@
 #   libraries provided by kliment.
 #   
 #########################################################################
-
 import socket
 import printcore
 import sys
 import time
+import thread
 
 from printrun.printcore import printcore
 from printrun.utils import setup_logging
@@ -27,23 +27,41 @@ from printrun import gcoder
 # The control board uses an SD card with stored GCode
 # The commands sent from the client will determine 
 # which code is run
-
+######################################################
 def sendcommand( command ):
     if ( command == "north" ) : 
         p.send_now("M23 north.gco")
         p.send_now("M24")
-    else if ( command == 'south' ) :
+    else if ( command == "south" ) :
         p.send_now("M23 south.gco")
         p.send_now("M24")
-    else if ( command == 'east' ) :
+    else if ( command == "east" ) :
         p.send_now("M23 east.gco")
         p.send_now("M24")
-    else if ( command == 'west' ) :
+    else if ( command == "west" ) :
         p.send_now("M23 west.gco")
         p.send_now("M24")
+    else if ( command == "disconnect" ) :
+        p.disconnect() 
+    else
 
 ######################################################
+# Receive in a thread to implement asynchronous 
+# communication.
+######################################################
+def receiver( socket, close ):
+    while True:
+        input = socket.recv()
+        if ( input == "exit" ):
+            close = True
+            socket.close()
+        else: 
+            sendcommand( command )       
 
+#####################################################
+# Begin script with logging to stderr and opening a 
+# connection to the control board
+#####################################################
 setup_logging(sys.stderr) 
 # Connect to robot
 p = printcore('/dev/ttyUSB0', 115200)       
@@ -58,11 +76,15 @@ s.bind((host,port))         # Bind to the port
 s.listen(5)                 # Now wait for client connection
 print '%s listening on port %s' % (host, port)
 
+# Create a boolean for testing if the client wants to close
+close = False
+
 while True :
     c, addr = s.accept()    # Establish connection with client
-    print 'Got connection from', addr
+    print "Got connection from", addr
+    thread.start_new_thread( receiver, ( c, close ) ) # Begin receiver thread
+    c.send("Server awaiting commands...")
     while True :
-        c.send('Server awaiting commands...')
-        if c.recv() == 'exit':
-            sys.exit(0)
+        if ( close == True ):
+            c.close()
 
